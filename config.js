c6d359e45b48ecad61077b6b7560c1fa7307cdaf
asimaliofficialq <awaseem9001@gmail.com>
2025-08-07 20:08:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK01zZ2lScVljMktYVlpNNndVb2JRcDZjYmozV05Ca0g0RkljdXc4WkQzND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNm93NTJHMFB2aE9BdlJMWVo4NGRBUlRSYXJTbURVenpPOGNlQkVSWm9XWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhQXU2LzhmcHJjakROeEJIUjVkVnU1ZUZtNDVLRUM3VkhOR2FiL1d1MDJVPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxa3hPdnRWdW9PRjBVN1ljV1laZXlESVlmSC8xTVRXb1VFU2lXVGZvYUc4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlJRmRjUW9iemVuSlY5L3RIY0FQcGs3N1Q3MGZaZ0xQNU1WYnZKQm1ZVzQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InE0U25SbTZRNzgvcGVWbkwxbHBsdjNZcHBKdWhJdmZ3RHFpbUh5a0NPZzA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUdVU3pCN0tLVm5YaFJsY3prMTV1UXAzcE5DZWhHU0l0b3NYMGRJZGJVVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTXpxdlJUVCt2aVBsNVQ1elBDSk5vYWhPTzBUQW14dnNYU1lLTEZUT05uRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkFnNDl3eWhxdTNySXl1ZGNxZmdmdkRzaVJ4c0FOaHU2NkJ4b1pEZVYweGs1eVBjbXJHTm9sOUplb2lraTA3SW5BWG00TEwvS2ZqRVlrRmRDUlU5V0JBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU3LCJhZHZTZWNyZXRLZXkiOiIvMEtNZ1RzN3p1UEZxZkZDT0cremFEaldjODhlQU95ZGhQbzlKd1RNV01zPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJOR0VMWTdNUiIsIm1lIjp7ImlkIjoiOTIzMjMwNzU1OTQyOjIyQHMud2hhdHNhcHAubmV0IiwibGlkIjoiMjIwMTMwNTQ2MDU3NDA5OjIyQGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTlR5eC93SEVMQ056c1FHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiRklOdnp5b3RkeEtTWitBQ2k2dVh0ZTAzUnBsczNnd2lScjlLVThJOFRBaz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiVnpBUDRTb3NwaWl0bTBFUnE1RmdPSmVuTitqeWVBZzlES0VCMmtYcEZDZnZHc1lheFB1OTJUZ0o0eityeFBOQlZhQTE3akswalRBOTRZUmg5UFlOQXc9PSIsImRldmljZVNpZ25hdHVyZSI6IkpaUEdaSU9HZWkreHBGa2JsL0N2VzJYeHpETTZJOERMOFRpVFdOV1VoeElBZUduMTUzYWxRS0U1bjM3TnB5YnllelVteEp6V0dWVno3c2tYc2NyS0RRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTIzMjMwNzU1OTQyOjIyQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlJTRGI4OHFMWGNTa21mZ0FvdXJsN1h0TjBhWmJONE1Ja2EvU2xQQ1BFd0oifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBZ0lCUT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1NDQ5ODc0MCwibGFzdFByb3BIYXNoIjoiMUs0aEg0In0=",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Arslan_MD 🇵🇰*",
 // set the auto reply massage on status reply  
@@ -23,7 +23,7 @@ WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
@@ -31,9 +31,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/yj7zp0.p
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "Arslan_MD",
+BOT_NAME: process.env.BOT_NAME || "ASIM-MD",
 // add bot namw here for menu
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // true to get auto status react
 STICKER_NAME: process.env.STICKER_NAME || "Arslan_MD",
 // type sticker pack name 
@@ -43,17 +43,17 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923230755942",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ArslanMD Official",
+OWNER_NAME: process.env.OWNER_NAME || "ASIMMD Official",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 Arslan_MD*",
+DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ASIM-MD*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yj7zp0.png",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*Arslan_MD*🇵🇰",
+LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*ASIM-MD*🇵🇰",
 // add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "true",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
@@ -75,10 +75,10 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923237045919",
+DEV: process.env.DEV || "923230755942",
 //replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
+ANTI_VV: process.env.ANTI_VV || "false",
 // true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
